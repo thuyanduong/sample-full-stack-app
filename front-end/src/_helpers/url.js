ac977994fa5ffde46f92f15867f83b03dcabb3b3
Ann Duong <thuyanannduong@gmail.com>
2021-05-26 22:31:19 -0400

    cleaned consoles

--- front-end/src/_helpers/url.js
+++ front-end/src/_helpers/url.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 
-console.log("weeooh", process.env.NODE_ENV)
-
 const URL = process.env.NODE_ENV === 
-  'production' ? 'https://anns-sample-back-end.herokuapp.com/' : 'http://localhost:4000'
+  'production' ? 'https://anns-sample-back-end.herokuapp.com' : 'http://localhost:4000'
 
 export default URL;
 
