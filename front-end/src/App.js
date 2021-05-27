b8aaec767efab8769fc75df6e13ec847f0ec562b
Ann Duong <thuyanannduong@gmail.com>
2021-05-26 22:28:46 -0400

    redeploy

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. 
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         {
           users.map(user => <div key={user.id}>{user.name}</div>)
