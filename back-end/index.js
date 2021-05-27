1d4f9b0b1455206a917c94281410057f6c5c1be4
Ann Duong <thuyanannduong@gmail.com>
2021-05-27 00:17:33 -0400

    made updates

--- back-end/index.js
+++ back-end/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.status(200).json({message: "This Ann's Express API"})
+  res.status(200).json({message: "I have made an update"})
 })
 
 app.get('/users', async (req, res) => {
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I have made an update
         </p>
         {
           users.map(user => <div key={user.id}>{user.name}</div>)
